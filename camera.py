bfb9864ad6493c1e9fc2bee7c5affa7cb0894696
mahdihbku <mla.mehdi@hotmail.com>
2020-01-11 22:59:23 +0300

    Add files via upload

--- camera.py
+++ camera.py
@@ -1,1 +1,175 @@
 #!/usr/bin/env python
+from openalpr import Alpr
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import cv2
+import time
+import argparse
+import os
+import pickle
+import sys
+import numpy as np
+import socket
+import ec_elgamal
+import os
+from multiprocessing import Pool
+from random import randint
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--img_dim',		type=int, help="Default image dimension.",						default=96				)
+parser.add_argument('--server_port',	type=int, help="Port of the server.",							default=6546			)
+parser.add_argument('--server_ip',		type=str, help="IP of the server.",								default='127.0.0.1'		)
+parser.add_argument('--cpus',			type=int, help="Number of parallel CPUs to be used.",			default=4				)
+parser.add_argument('--sensitivity',	type=int, help="Non-matching characters in the plate (0,1,2).",	default=2				)
+parser.add_argument('--generate_keys',	action='store_true', help="Generate new server keys."									)
+parser.add_argument('--verbose',		action='store_true', help="Output more details."										)
+parser.add_argument('--capture_device',	type=int, default=0, help='Capture device. 0 for latop webcam and 1 for usb webcam'		)
+parser.add_argument('--load', 			action='store_true'																		)
+args = parser.parse_args()
+
+server_ip 		= args.serverIP
+server_port		= args.serverPort
+nbr_of_cpus		= args.cpus
+verbose			= args.verbose
+DB_file 		= "rec_DB.data"
+pub_key_file 	= "rec_pub.txt"
+elgamal_ct_size = 130
+load 			= args.load
+sensitivity 	= args.sensitivity
+rand_nbrs_file	= "rand_num.data"
+rand_nbrs_min_bitlen = 128
+rand_nbrs_max_bitlen = 128
+
+# Temporary global variables
+DB = []
+G = []
+
+def connect_to_server():
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server_address = (server_ip, server_port)
+	if verbose:	print("connectToServer: Connecting to {}:{}...".format(server_ip, server_port))
+	sock.connect(server_address)
+	if verbose:	print("connectToServer: Connected")
+	return sock
+
+def get_pub_key(sock):
+	try:
+		if verbose:	print("getPubKey: Getting the server's key...")
+		message = 'GET pub_key'
+		sock.sendall(message)
+		pub_key = recv_msg(sock)
+		f = open(pub_key_file, "w")
+		f.write(pub_key)
+		f.close()
+		if verbose:	print("getPubKey: Key received")
+	except:
+		print("getPubKey: Error")
+
+def get_DB_file(sock):
+	try:
+		if verbose:	print("getBCfiles: Getting Encrypted DB...")
+		message = "GET DBfiles"
+		sock.sendall(message)
+		DB = recv_msg(sock)
+		f = open(DB_file, 'w')
+		f.write(DB)
+		f.close()
+		if verbose:	print("getBCfiles: Encrypted DB received")
+	except:
+		print("getBCfiles: Error")
+
+def fill_rand_numbers_file():
+	if verbose:	print("fill_rand_numbers_file: Generating the random-numbers file...")
+	DB = np.load(DB_file)
+	plates_count = len(DB)
+	ec_elgamal.prepare_for_enc(pub_key_file)
+	f = open(rand_nbrs_file, "wb")
+	for i in range(plates_count):
+		rand1 = random.getrandbits(randint(rand_nbrs_min_bitlen, rand_nbrs_max_bitlen))
+		rand2 = random.getrandbits(randint(rand_nbrs_min_bitlen, rand_nbrs_max_bitlen))
+		if (rand1 < rand2):
+			rand1, rand2 = rand2, rand1		# always rand1 > rand2
+		f.write(str(rand1)+'\n')
+		f.write(ec_elgamal.encrypt_ec(str(rand2)))
+	f.close()
+	if verbose:	print("fill_rand_numbers_file: Random-numbers file generated")
+
+def send_msg(sock, msg):
+	msg = struct.pack('>I', len(msg)) + msg
+	sock.sendall(msg)
+
+def recv_msg(sock):
+	raw_msglen = recvall(sock, 4)
+	if not raw_msglen:
+		return None
+	msglen = struct.unpack('>I', raw_msglen)[0]
+	return recvall(sock, msglen)
+
+def recvall(sock, n):
+	data = b''
+	while len(data) < n:
+		packet = sock.recv(n - len(data))
+		if not packet:
+			return None
+		data += packet
+	return data
+
+def encrypt_for_G(list):
+	return [[ec_elgamal.encrypt_ec(str(g*10**(k*2))) for k in range (8)] for g in list]
+
+def generate_local_files():
+	start_gen_G = time.time()
+	ec_elgamal.prepare_for_enc(pub_key_file)
+	if verbose:	print("generate_local_files: Generating local G...")
+	pool = Pool(processes=nbr_of_CPUs)
+	G_values = range(100)
+	G = pool.map(encrypt_for_G, (G_values[int(i*100/nbr_of_CPUs):int((i+1)*100/nbr_of_CPUs)] for i in range(nbr_of_CPUs)))
+	G = [ent for sublist in G for ent in sublist]
+	np.save(G_file, G)
+	del G
+	pool.close()
+	end_gen_G = time.time()
+	if verbose:	print("generate_local_files: G generated in: {} ms".format((end_gen_G-start_gen_G)*1000))
+	# results_file = open("camera_results.txt","a+")
+	# storage = ec_elgamal_ct_size*(128*suspects_count*256/G_portion+128*suspects_count+256+suspects_count)+256*suspects_count if G_portion>0 else ec_elgamal_ct_size*(128*suspects_count+128*suspects_count+256+suspects_count)+256*suspects_count
+	# storage = storage*1.00/1024/1024
+	# results_file.write("Offile:M= {} CPUs_camera= {} F_G_gen= {} G_portion= {} storage((GorC)+B+F+rand)= {}\n".format(suspects_count,nbr_of_CPUs,end_gen_files-start_gen_files,G_portion,storage))
+	# results_file.close()
+
+def encode_plate_number(detected_plate):
+	encoded_plate = []
+	for char in detected_plate:
+		if char not in ['\n', '\t']:
+			encoded_plate.append(str(ord(char)-26))	# max ascii code 126
+	for i in range(len(encoded_plate), plate_size):
+		encoded_plate.append('99')
+	return "".join(encoded_plate)
+
+def sendScores(connection, scores):
+	try:
+		message = "new_scores "
+		connection.sendall(message)
+		send_msg(connection, scores)
+	except:
+		print ("sendScores: Error")
+
+if __name__ == '__main__':
+	sock = connectToServer()
+	if not load:
+		# Offline phase___________________________________________
+		get_pub_key(sock)
+		if verbose:	print("main: pub_key received and saved in {}".format(pub_key_file))
+		get_DB_file(sock)
+		if verbose:	print("main: Encrypted DB received and saved in {}, {}".format(DB_file))
+		generate_local_files()
+		if verbose:	print("main: Local files have been generated")
+		fill_rand_numbers_file()
+		if verbose:	print("main: random files file {} has been filled".format(rand_numbers_file))
+
+	# Online phase____________________________________________
+	ec_elgamal.prepare_for_enc(pub_key_file)
+	DB = np.load(DB_file)
+	G = np.load(str(G_file+".npy"))
+	plates_count = len(DB)
+
+	#stopped here: -read plate nbr from cli or from camera? -when computing the scores we have to consider the sensitivity. based on it we have to figure out how to compute it!!
--- server.py
+++ server.py
@@ -6,29 +6,32 @@ import pickle
 import numpy as np
 import socket
 import ec_elgamal
+import os
 from multiprocessing import Pool
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--cars_file',		type=str, help="Path to file containing list of cars' plates.",	default=""	)
 parser.add_argument('--cars_folder',	type=str, help="Path to folder containing pictures of cars.",	default=""			)
-parser.add_argument('--imgDim',			type=int, help="Default image dimension.",						default=96				)
-parser.add_argument('--serverPort',		type=int, help="Port of the server.",							default=6546			)
-parser.add_argument('--serverIP',		type=str, help="IP of the server.",								default='127.0.0.1'		)
-parser.add_argument('--CPUs',			type=int, help="Number of parallel CPUs to be used.",			default=4				)
-parser.add_argument('--sensitivity',	type=int, help="Non-matching characters in the plate (0,1,2).",	default=2				)
-parser.add_argument('--generateKeys',	action='store_true', help="Generate new server keys."									)
+parser.add_argument('--img_dim',		type=int, help="Default image dimension.",						default=96				)
+parser.add_argument('--server_port',	type=int, help="Port of the server.",							default=6546			)
+parser.add_argument('--server_ip',		type=str, help="IP of the server.",								default='127.0.0.1'		)
+parser.add_argument('--cpus',			type=int, help="Number of parallel CPUs to be used.",			default=16				)
+# parser.add_argument('--sensitivity',	type=int, help="Non-matching characters in the plate (0,1,2).",	default=2				)
+parser.add_argument('--generate_keys',	action='store_true', help="Generate new server keys."									)
 parser.add_argument('--verbose',		action='store_true', help="Output more details."										)
 args = parser.parse_args()
 
-server_ip 		= args.serverIP
-server_port		= args.serverPort
-nbr_of_CPUs		= args.CPUs
+server_ip 		= args.server_ip
+server_port		= args.server_port
+nbr_of_cpus		= args.cpus
 verbose			= args.verbose
 cars_file		= args.cars_file
 cars_folder		= args.cars_folder
-sensitivity 	= args.sensitivity
+# sensitivity 	= args.sensitivity
 pub_key_file	= "ec_pub.txt"
 priv_key_file	= "ec_priv.txt"
+plate_size		= 8					# max number of chars in the plate
+DB_file 		= 'DB.data'
 
 def send_pub_key(connection):
 	f = open(pub_key_file, 'r')
@@ -37,16 +40,11 @@ def send_pub_key(connection):
 	if verbose:	print("sendPubKey: Public key sent")
 	f.close()
 
-def send_DB_files(connection):
-	f = open(str(B_file+".npy"), "rb")
-	B = f.read()
-	send_msg(connection, B)
-	if verbose:	print("sendDBfiles: B sent")
-	f.close()
-	f = open(str(C_file+".npy"), "rb")
-	C = f.read()
-	send_msg(connection, C)
-	if verbose:	print("sendDBfiles: C sent")
+def send_enc_DB(connection):
+	f = open(str(DB_file+".npy"), "rb")
+	DB = f.read()
+	send_msg(connection, DB)
+	if verbose:	print("sendDBfiles: Encrypted DB sent")
 	f.close()
 
 def wait_for_clients():
@@ -66,7 +64,7 @@ def wait_for_clients():
 					if (data == "GET pub_key"):
 						send_pub_key(connection)
 					if (data == "GET DBfiles"):
-						send_DB_files(connection)
+						send_enc_DB(connection)
 					if (data == "new_scores "):
 						get_scores(connection)
 				else:
@@ -77,88 +75,100 @@ def wait_for_clients():
 			print("waitForClients: Connection closed with client {}".format(client_address))
 
 def get_scores(connection):
-	try:
-		ec_elgamal.prepare(pub_key_file, priv_key_file)
-		data = recv_msg(connection)
-		enc_D = pickle.loads(data)
+	cccc=0
+	# try:
+	# 	ec_elgamal.prepare(pub_key_file, priv_key_file)
+	# 	data = recv_msg(connection)
+	# 	enc_D = pickle.loads(data)
 
-		start_dec = time.time()
-		D = [ec_elgamal.dec_zero_nonzero(encrypted_score) for encrypted_score in enc_D]	#if decrypted_score == 0 return 0, else return 1
-		end_dec = time.time()
-		if verbose:	print("getScores: dec_time for {} suspects: {} ms.".format(len(D), (end_dec-start_dec)*1000))
-		if verbose:	print(D)
+	# 	start_dec = time.time()
+	# 	D = [ec_elgamal.dec_zero_nonzero(encrypted_score) for encrypted_score in enc_D]	#if decrypted_score == 0 return 0, else return 1
+	# 	end_dec = time.time()
+	# 	if verbose:	print("getScores: dec_time for {} suspects: {} ms.".format(len(D), (end_dec-start_dec)*1000))
+	# 	if verbose:	print(D)
 
-		results_file = open("final_results.txt", "a+")
-		results_file.write("Online:dec_time= {}\n".format((end_dec-start_dec)*1000))
-		results_file.close()
-		## stopped here......................................
-		# i=0
-		# while (i<len(D) and len(D[i])<200):		# TODO: uncomment !!
-		# 	i+=1									# TODO: uncomment !!
-		if (0 in D):	# SUSPECT DETECTED!!!
-			print("getScores: SUSPECT DETECTED! id={} name={}".format(i, suspects_names[i]))
-			message = "GET image  "
-			connection.sendall(message)
-			data = recv_msg(connection)
-			now = datetime.datetime.now()
-			image_name = "suspect"+str(now.strftime("%Y-%m-%d-%H-%M")+".png")
-			frame = pickle.loads(data)
-			cv2.imwrite(image_name, frame)
-			print("getScores: Suspect's image saved in {}".format(image_name))
-		else:
-			message = "No match"
-			connection.sendall(message)
-	except:
-		print 'getScores: Error'
+	# 	results_file = open("final_results.txt", "a+")
+	# 	results_file.write("Online:dec_time= {}\n".format((end_dec-start_dec)*1000))
+	# 	results_file.close()
+	# 	## stopped here......................................
+	# 	# i=0
+	# 	# while (i<len(D) and len(D[i])<200):		# TODO: uncomment !!
+	# 	# 	i+=1									# TODO: uncomment !!
+	# 	if (0 in D):	# SUSPECT DETECTED!!!
+	# 		print("getScores: SUSPECT DETECTED! id={} name={}".format(i, suspects_names[i]))
+	# 		message = "GET image  "
+	# 		connection.sendall(message)
+	# 		data = recv_msg(connection)
+	# 		now = datetime.datetime.now()
+	# 		image_name = "suspect"+str(now.strftime("%Y-%m-%d-%H-%M")+".png")
+	# 		frame = pickle.loads(data)
+	# 		cv2.imwrite(image_name, frame)
+	# 		print("getScores: Suspect's image saved in {}".format(image_name))
+	# 	else:
+	# 		message = "No match"
+	# 		connection.sendall(message)
+	# except:
+	# 	print 'getScores: Error'
 
 def encode_plate_number(detected_plate):
-	# stopped here: in: unicode u'786P0J', out: encoded plate number (ascii-26 of each char)
+	encoded_plate = []
+	for char in detected_plate:
+		if char not in ['\n', '\t']:
+			encoded_plate.append(str(ord(char)-26))	# max ascii code 126
+	for i in range(len(encoded_plate), plate_size):
+		encoded_plate.append('99')
+	return "".join(encoded_plate)
 
 def generate_DB_files():
-	start_norm = time.time()
+	start_recognition = time.time()
 	ec_elgamal.prepare(pub_key_file, priv_key_file)
+	plates = []
 	if (cars_folder != ""):
-		for root, dirs, files in os.walk(args.suspectsDir):
+		alpr = Alpr("us", "/etc/openalpr/openalpr.conf", "/usr/share/openalpr/runtime_data")
+		for root, dirs, files in os.walk(cars_folder):
 			for img in files:
-				alpr = Alpr("us", "/etc/openalpr/openalpr.conf", "/usr/share/openalpr/runtime_data")
-				results = alpr.recognize_file(img)
-	else if (cars_file != "") :
-
+				results = alpr.recognize_file(os.path.join(root, img))
+				if results.values()[5] != []:
+					plates.append(results.values()[5][0].values()[0])			# get the result with the best confidence
+	elif (cars_file != "") :
+		if not os.path.isfile(cars_file):
+			print("generate_DB_files: ERROR! File {} does not exist!".format(cars_file))
+			exit(0)
+		with open(cars_file) as fp:
+			for line in fp:
+				plates.append(line)
 	else :
 		print("generate_DB_files: ERROR! No source file/directory specified")
-
-	suspects_reps = []
-	suspects_names[:] = []
-	if verbose:	print("generate_DB_files: Detecting and normalizing faces...")
-	for root, dirs, files in os.walk(args.suspectsDir):
-		for img in files:
-			imgrep = getRep(os.path.join(root, img))
-			if imgrep == '': continue
-			suspects_reps.append(normalizeRep(imgrep))
-			suspects_names.append(os.path.join(root, img))
-	end_norm = time.time()
-	if verbose:	print("generate_DB_files: Suspects in dir {} have been normalized in {}".format(args.suspectsDir, (end_norm-start_norm)*1000))
-
+		exit(0)
+	encoded_plates = [encode_plate_number(plate) for plate in plates]
+	end_recognition = time.time()
+	if verbose:	print("generate_DB_files: Plates generated in {} ms".format((end_recognition-start_recognition)*1000))
+	# print("generate_DB_files: plates:")
+	# print plates
+	print("generate_DB_files: encoded_plates:")
+	print encoded_plates
 	start_enc = time.time()
-	if verbose:	print("generate_DB_files: Generating matrix B...")
-	pool = Pool(processes=nbr_of_CPUs)
-	B = pool.map(encryptForB, (suspects_reps[int(i*len(suspects_reps)/nbr_of_CPUs):int((i+1)*len(suspects_reps)/nbr_of_CPUs)] for i in range(nbr_of_CPUs)))
-	B = [ent for sublist in B for ent in sublist]
-	if verbose:	print("generate_DB_files: B generated")
-	if verbose:	print("generate_DB_files: Generating matrix C...")
-	C = pool.map(encryptForC, (suspects_reps[int(i*len(suspects_reps)/nbr_of_CPUs):int((i+1)*len(suspects_reps)/nbr_of_CPUs)] for i in range(nbr_of_CPUs)))
-	pool.close()
-	C = [ent for sublist in C for ent in sublist]
-	if verbose:	print("generate_DB_files: C generated")
+	if verbose:	print("generate_DB_files: Generating encrypted DB...")
+	pool = Pool(processes=nbr_of_cpus)
+	DB = pool.map(encrypt_for_DB, (encoded_plates[int(i*len(encoded_plates)/nbr_of_cpus):int((i+1)*len(encoded_plates)/nbr_of_cpus)] for i in range(nbr_of_cpus)))
+	DB = [ent for sublist in DB for ent in sublist]
 	end_enc = time.time()
-	print("generate_DB_files: DB files generated in: {} ms.".format((end_enc-start_enc)*1000))
+	if verbose:	print("generate_DB_files: Encrypted DB generated in: {} ms".format((end_enc-start_enc)*1000))
+	np.save(DB_file, DB)
 
-	results_file = open("final_results.txt", "a+")
-	results_file.write("Offline:M= {} CPUs_srvr= {} ident+norm= {} BCgen= {} storage(B+C+keys)= {} off_comm= {} onl_comm= {}\n".format(len(suspects_reps), nbr_of_CPUs, end_norm-start_norm, end_enc-start_enc, 2*len(suspects_reps)*128*512*1.00/1024/1024, 2*len(suspects_reps)*128*512*1.00/1024/1024, len(suspects_reps)*512*1.00/1024))
-	results_file.close()
+	# results_file = open("server_results.txt", "a+")
+	# results_file.write("Offline:M= {} CPUs_srvr= {} ident+norm= {} BCgen= {} storage(B+C+keys)= {} off_comm= {} onl_comm= {}\n".format(len(encoded_plates), nbr_of_cpus, end_recognition-start_recognition, end_enc-start_enc, 2*len(encoded_plates)*128*512*1.00/1024/1024, 2*len(encoded_plates)*128*512*1.00/1024/1024, len(encoded_plates)*512*1.00/1024))
+	# results_file.close()
 
-	np.save(B_file, B)
-	np.save(C_file, C)
+def encrypt_for_DB(list):
+	enc_plates = []
+	for plate in list:
+		enc_plate = []
+		for i in range(0, len(plate)-1, 2):
+			enc_plate.append(ec_elgamal.encrypt_ec(str(int(plate[i:i+2])*10**i)))
+			# print("encrypt_for_DB: encrypting: {}".format(str(int(plate[i:i+2])*10**i)))
+		enc_plates.append(enc_plate)
+	return enc_plates
 
 def send_msg(sock, msg):
 	msg = struct.pack('>I', len(msg)) + msg
@@ -181,7 +191,7 @@ def recvall(sock, n):
 	return data
 
 if __name__ == '__main__':
-	if args.generateKeys :
+	if args.generate_keys :
 		ec_elgamal.generate_keys(pub_key_file, priv_key_file)
 	generate_DB_files()
 	wait_for_clients()
